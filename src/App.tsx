0056798abf092cd582767fdcd339ed34b7334d05
Eder Gabriel <eder1602@outlook.com>
2021-01-04 23:54:40 -0300

    Página de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import './App.css';
 
+import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn'
 const App: React.FC = () => (
-  <h1>Hello World</h1>
+  <>
+  <SignIn />
+  <GlobalStyle />
+  </>
 )
 
 export default App;
