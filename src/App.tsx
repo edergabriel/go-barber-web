14520f9a94c6f2f718a90647b5f50c19951a1414
Eder Gabriel <eder1602@outlook.com>
2021-01-19 19:44:30 -0300

    Estrutura de toasts

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 import { AuthProvider } from './hooks/AuthContext'
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
     <GlobalStyle />
   </>
 )
